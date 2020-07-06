eaef99ee8e9d94b0d6a83a297fc09c8eceb460c3
ishotjr <github@ishotjr.com>
2020-07-05 23:55:10 -0400

    :radio_button: :white_check_mark: Add selectable Completed

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,7 @@ import {
 	useGlobalConfig,
 	expandRecord,
 	TablePickerSynced,
+	FieldPickerSynced,
 	TextButton,
 } from '@airtable/blocks/ui';
 import React from 'react';
@@ -14,24 +15,29 @@ function TasksBlock() {
 
 	const globalConfig = useGlobalConfig();
 	const tableId = globalConfig.get('selectedTableId');
+	const completedFieldId = globalConfig.get('completedFieldId');
 
 	const table = base.getTableByIdIfExists(tableId);
+	const completedField = table ? table.getFieldByIdIfExists(completedFieldId) : null;
 
 	const records = useRecords(table);
 
-	const tasks = records ? records.map(record => {
-		return <Task key={record.id} record={record} />;
-	}) : null;
+	const tasks = records && completedFieldId ? records.map(record => (
+		<Task key={record.id} record={record} completedFieldId={completedFieldId} />
+	)) : null;
 
     return (
 		<div>
 			<TablePickerSynced globalConfigKey="selectedTableId" />
+			<FieldPickerSynced table={table} globalConfigKey="completedFieldId" />
 			{tasks}
 		</div>
 	);
 }
 
-function Task({record}) {
+function Task({record, completedFieldId}) {
+	const label = record.name || 'Unnamed record';
+
 	return (
 		<div
 			style={{
@@ -43,7 +49,7 @@ function Task({record}) {
 				borderBottom: '1px solid #ddd',
 			}}
 		>
-			{record.name || 'Unnamed record'}
+			{record.getCellValue(completedFieldId) ? <s>{label}</s> : label}
 			<TextButton
 				icon="expand"
 				aria-label="Expand record"
