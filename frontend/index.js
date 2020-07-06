838d62db099610fd0cb77ccdb13e832a36f5f4f2
ishotjr <github@ishotjr.com>
2020-07-05 21:55:40 -0400

    :radio_button: :books: Add TablePicker, remove hard-coding

--- frontend/index.js
+++ frontend/index.js
@@ -3,21 +3,35 @@ import {
 	useBase,
 	useRecords,
 	expandRecord,
+	TablePicker,
 	TextButton,
 } from '@airtable/blocks/ui';
-import React from 'react';
+import React, {useState} from 'react';
 
 function TasksBlock() {
 	const base = useBase();
-	const table = base.getTableByName('Tasks');
+
+	const [tableId, setTableId] = useState(null);
+
+	const table = base.getTableByIdIfExists(tableId);
 
 	const records = useRecords(table);
 
-	const tasks = records.map(record => {
+	const tasks = records ? records.map(record => {
 		return <Task key={record.id} record={record} />;
-	})
+	}) : null;
 
-    return <div>{tasks}</div>;
+    return (
+		<div>
+			<TablePicker
+				table={table}
+				onChange={newTable => {
+					setTableId(newTable.id);
+				}}
+			/>
+			{tasks}
+		</div>
+	);
 }
 
 function Task({record}) {
